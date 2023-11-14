b7993b802a18ddb4eae83c96188d5506aba829a6
Mariano Labatut <marianolabatut@gmail.com>
2023-11-14 16:58:46 -0300

    Animaciones

---
+++ animaciones.c
@@ -0,0 +1,19 @@
+#include "animaciones.h"
+
+void animacionCargando()
+{
+    printf("\n\n\n                                                 Cargando");
+    systemix();
+    printf("\n\n\n                                                 Cargando.");
+    systemix();
+    printf("\n\n\n                                                 Cargando..");
+    systemix();
+    printf("\n\n\n                                                 Cargando...");
+    systemix();
+}
+
+void systemix()
+{
+    Sleep(300);
+    system("cls");
+}
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "animaciones.h"
 
 int main()
 {
-    printf("Hello world!\n");
     return 0;
 }
